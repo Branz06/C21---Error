87fea552784f229ef2cc95205f8044da36c25daf
Branz06 <112098371+Branz06@users.noreply.github.com>
2022-10-16 14:46:23 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,50 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var ball;
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+var ball_options={
+	isStatic:false,
+	restitution:0.3,
+	friction:0,
+	density:1.2
+}
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+
+	ball = Bodies.circle(ball_options);
+    World.add(world,ball);
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  groundObj.display();
+  groundObj=new ground(width/2,670,width,20);
+  lifeSide = new ground(1100,600,20,120);
+  drawSprites();
+ 
+}
+
+function keypressed(){
+	if(keyCode===UP_ARROW){
+		 Matter.Body.applyForce(ball,{x:0,y:0},{x:0,y:-0.05})
+	}
+}
+
+
